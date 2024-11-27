ac258e806418989ff750e27656b17a6524ca83bc
teltelbozz <nobuty717@gmail.com>
2024-11-28 00:55:05 +0900

    Update vacant-houses-data.js

--- api/vacant-houses-data.js
+++ api/vacant-houses-data.js
@@ -19,21 +19,7 @@ export default async function handler(req, res) {
     if (!houseType || !buildingType || !decayStatus) {
       return res.status(400).json({ error: 'Missing required query parameters' });
     }
-
-           // "すべて" の処理
-        const houseTypes = houseType === "（すべて）"
-            ? "'一戸建','長屋建','共同住宅','その他'" // すべてのオプション
-            : `$1`; // プレースホルダを使用
-
-        const buildingTypes = buildingType === "（すべて）"
-            ? "'木造','鉄骨造','鉄筋コンクリート造'" // 仮の例: 建物構造のすべてのオプション
-            : `$2`;
-
-        const decayStatuses = decayStatus === "(すべて)"
-            ? "'腐朽あり','腐朽なし'" // 仮の例: 腐朽破損のすべてのオプション
-            : `$3`;
-
-
+    
     // クエリを実行して集計データを取得
     const query = `
       SELECT 
@@ -41,18 +27,16 @@ export default async function handler(req, res) {
         SUM(空き家数) AS emptyhouse
       FROM vacant_houses
       WHERE 
-        腐朽破損有無 IN(${decayStatuses}) AND
-        住宅建て方 IN(${houseTypes}) AND 
-        建物構造 IN(${buildingTypes})
+        腐朽破損有無 IN($3) AND
+        住宅建て方 IN($1) AND 
+        建物構造 IN($2)
       GROUP BY 地域
       ORDER BY emptyhouse ASC;
     `;
 
      console.log(query)
 
-    const values = houseType === "(すべて)" || buildingType === "(すべて)" || decayStatus === "(すべて)"
-            ? [] // すべてが選択された場合は値は不要
-            : [houseType, buildingType, decayStatus];
+    const values = [houseType, buildingType, decayStatus];
     const result = await pool.query(query, values);
     
     // 必要な形式にデータを整形
