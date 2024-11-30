a2d3c22fa31188b9b1c09b3715a8b27f8fd67272
teltelbozz <nobuty717@gmail.com>
2024-11-30 15:52:36 +0900

    Add files via upload

--- api/vacant-houses-data.js
+++ api/vacant-houses-data.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
     if (!houseType || !buildingType || !decayStatus) {
       return res.status(400).json({ error: 'Missing required query parameters' });
     }
-    
+
     // クエリを実行して集計データを取得
     const query = `
       SELECT 
@@ -27,18 +27,32 @@ export default async function handler(req, res) {
         SUM(空き家数) AS emptyhouse
       FROM vacant_houses
       WHERE 
-        腐朽破損有無 IN($3) AND
-        住宅建て方 IN($1) AND 
-        建物構造 IN($2)
+        腐朽破損有無 IN('総数') AND
+        住宅建て方 IN('一戸建') AND 
+        建物構造 IN('木造')
       GROUP BY 地域
       ORDER BY emptyhouse ASC;
     `;
-
-     console.log(query)
-
+/*
+    const query = `
+    SELECT 
+      地域 AS locationName,
+      SUM(空き家数) AS emptyhouse
+    FROM vacant_houses
+    WHERE 
+      腐朽破損有無 IN($3) AND
+      住宅建て方 IN($1) AND 
+      建物構造 IN($2)
+    GROUP BY 地域
+    ORDER BY emptyhouse ASC;
+  `;1
+*/
     const values = [houseType, buildingType, decayStatus];
+    console.error(values)
+    console.log(values)
     const result = await pool.query(query, values);
-    
+
+
     // 必要な形式にデータを整形
     const formattedResult = result.rows.map(row => ({
       locationName: row.locationname,
